e1ffeea0256961b8fe25a18f08e5a67e06d5bbfb
Taewoo <skaxodn97@gmail.com>
2020-11-07 16:00:30 +0900

    게시판 만들기 폴더구조 설정

---
+++ project/src/api/posts/routes/index.js
@@ -0,0 +1,49 @@
+const postRouter = require('express').Router();
+
+/**
+ * todo: 페이지네이션
+ * @description 게시물 리스트 조회
+ * @routes GET / posts
+ */
+postRouter.get('/' );
+
+/**
+ * todo: 페이지네이션
+ * @description 게시물 댓글 리스트 조회
+ * @routes GET / posts/:id/comments
+ */
+postRouter.get('/:id/comments' );
+
+/**
+ * @description 게시물 조회
+ * @routes GET / posts/:id
+ */
+postRouter.get('/:id' );
+
+/**
+ * @description 게시물 등록
+ * @routes POST / posts
+ * @request @body {content, images}
+ */
+postRouter.post('/');
+
+/**
+ * @description 게시물 수정
+ * @routes PUT / posts/:id
+ * @request @body {content, images}
+ */
+postRouter.put('/:id');
+
+/**
+ * @description 게시물 삭제
+ * @routes DELTE / posts/:id
+ */
+postRouter.delete('/:id');
+
+/**
+ * @description 게시물 좋아요
+ * @routes PATCH / posts/:id/like
+ */
+postRouter.patch('/:id/like');
+
+module.exports = postRouter;
---
+++ project/src/loaders/router.loader.js
@@ -0,0 +1,19 @@
+const commentRouter = require("../api/comments/routes");
+const postRouter = require("../api/posts/routes");
+const userRouter = require("../api/users/routes");
+
+const loadRouter = (app)=>{
+
+    app.use('posts', postRouter);
+    app.use('users', userRouter);
+    app.use('comments', commentRouter);
+    // 존재하지 않는 경로 처리
+    app.use((req,res,next)=>{
+        res.status(404).json({
+            success: false,
+            message: "Not Found"
+        });
+    });
+};
+
+module.exports = loadRouter;
